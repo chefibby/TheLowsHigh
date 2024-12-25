590d4a5f76b39c3e722b3c94b96bc9cf88041e1e
chefibby <lifequran9@gmail.com>
2024-12-25 00:11:33 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Select the button and the paragraph
-const button = document.getElementById("myButton");
-const message = document.getElementById("message");
-
-// Add a click event to the button
-button.addEventListener("click", () => {
-    message.textContent = "Hello! You clicked the button!";
-});
